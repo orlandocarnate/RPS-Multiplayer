5811f49d2bd9c1eae40b9541f10ab987b807e510
Orlando Carnate <orlando.carnate@gmail.com>
2019-01-16 15:58:32 -0600

    First commit, sketch layout and pseudocode.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RPS-Multiplayer
  An Online multiplayer Rock Paper Scissors game using Firebase
+
+## Game mechanics
+*Short Summary of Rock, Paper, Scissors.
+* Game is over if a player wins 2 out of 3 games.
+* Game is over after 5 minutes of idle time (no activity from either player).
+
+* Whoever selects first will have their buttons grayed out and not active.
+* When second player chooses, their buttons are grayed out and the game object will determine the winner.
+* A 3 second timer countdowns to reveal who won and show the buttons that were selected.
+* Winner gets a point.
+* After 10 seconds the game continues.
+* When the game is over or if no activity after a period of time, the history will update and each player's score will be reset to 0.
---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+// ----- start -----
+
+// declare variable
+var history = []; // game history that will retrieve data from Firebase
+
+var p1score = 0;
+var p2score = 0;
+
+// Event Listeners
+
+// if player selects a button
+
+
+// if player presses chat submit button
+
+
+// if game is idle (no one is playing) for more than 5 minutes game is over.
+
+
+
+// rps game object
+var rpsGame = {
+    // compare player 1 and 2 selections and see who the winner is
+    checkWinner: function(p1, p2) {
+        // insert alogorithm
+
+        // return result
+        return winner;
+    }
+
+    // update footer w history data
+    history: function () {
+        // // load any saved values from Firebase to history array
+
+        // update DOM
+    }
+}
+
+
+//------ end ------
+});
